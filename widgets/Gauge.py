431d6c87dede04472cefa00a3477570b33f3d6d1
Turkey <turkey2349@gmail.com>
2018-09-04 23:34:04 -0400

    Adding the ability to change the dial position of the gauge

--- widgets/Gauge.py
+++ widgets/Gauge.py
@@ -7,19 +7,23 @@ class Gauge:
     def __init__(self, frame, low, high, tick, subtick):
         self.width = 250
         self.height = 250
-        self.half_width = self.width /2
-        self.half_hegiht = self.height /2
+        self.half_width = self.width / 2
+        self.half_hegiht = self.height / 2
+        self.high = high
+        self.low = low
+        self.tick = tick
+        self.subtick = subtick
 
         self.canvas = tk.Canvas(frame, bg="#282a36", height=self.height, width=self.width, borderwidth=0)
 
         self.canvas.create_oval(0, 0, self.width, self.height, fill="#282a36", width=2, outline='red')
 
-        self.canvas.create_line(self.width/2, self.height/2, 200, 25, fill='red', arrow=tk.LAST, arrowshape=(130, 90, 10))
+        self.dial = self.canvas.create_line(self.width/2, self.height/2, 200, 25, fill='red', arrow=tk.LAST, arrowshape=(130, 90, 10))
 
-        arc_low = 140
-        arc_high = 410
+        self.arc_low = 140
+        self.arc_high = 410
         val = low
-        for theta in range(arc_low, arc_high, int((arc_high-arc_low)/((high-low + 1)/subtick))):
+        for theta in range(self.arc_low, self.arc_high, int((self.arc_high-self.arc_low)/((high-low + 1)/subtick))):
             theta_rad = math.radians(theta)
             x = math.cos(theta_rad)
             y = math.sin(theta_rad)
@@ -43,4 +47,13 @@ class Gauge:
 
             val += subtick
 
+        self.setDialValue(0)
+
         self.canvas.pack()
+
+    def setDialValue(self, val):
+        mult = val * (self.arc_high - self.arc_low) / ((self.high - self.low + 1) / self.subtick)
+        theta_rad = math.radians(mult)
+        x = math.cos(theta_rad)
+        y = math.sin(theta_rad)
+        self.canvas.coords(self.dial, self.width/2, self.height/2, (x * 25) + self.width/2, (y * 25) + self.height/2)
