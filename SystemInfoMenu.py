0b8686149959fc188d1a5c0e78c5d172ab5260d0
Turkey2349 <turkey2349@gmail.com>
2018-09-12 18:16:24 -0400

    Fixed crash issues with bad COM Ports

--- SystemInfoMenu.py
+++ SystemInfoMenu.py
@@ -1,26 +1,36 @@
 import tkinter as tk
 import obd
 import widgets.Gauge as gauge
+import serial
 
 
 class SystemInfoMenu(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
+        self.connection = None
         self.controller = controller
         self.connect_odb()
 
     def connect_odb(self):
         #TODO: Add some sort of loading signal
+        ports = obd.scan_serial()
+        for port in ports:
 
-        self.connection = obd.OBD()
-        if self.connection.is_connected():
+            passes = True
+            try:
+                self.connection = obd.OBD(port)
+            except serial.serialutil.SerialException:
+                passes = False
+
+            if passes:
+                break
+
+        if self.connection is not None and self.connection.is_connected():
             self.add_obd_widgets()
         else:
+            print("Failed to find OBD2 device! Retrying in 10 seconds")
             self.controller.after(10000, self.connect_odb)
 
     def add_obd_widgets(self):
         self.test_gauge = gauge.Gauge(self, 0, 100, 10, 1)
-
-        for item in self.connection.supported_commands:
-            print(item)
